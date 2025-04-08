416c4a99cdb4be770c8cbd8c17555a1c30d54346
AnonioZ <anton.zubaha@gmail.com>
2025-04-08 21:04:24 +0300

    Updated IntroSection with dark elegant background

--- src/components/sections/MissionSection.tsx
+++ src/components/sections/MissionSection.tsx
@@ -18,14 +18,30 @@ const MissionSection = () => {
       <div className="container">
         <h2 className="mission-headline gradient-headline">Our Mission</h2>
         
-        <div className="mission-content">
+        <div className="mission-content" style={{ position: 'relative' }}>
           <div className="mission-logo-container">
             <img src="/images/Logo.png" alt="FTDiam Logo" className="mission-logo" />
           </div>
           
           <div className="mission-text">
-            <div className="quote-mark" dangerouslySetInnerHTML={{ __html: "&ldquo;" }}></div>
-            <p className="mission-description">
+            <img 
+              src="/images/quote.svg" 
+              alt="" 
+              aria-hidden="true"
+              style={{ 
+                position: "absolute",
+                left: "-20px", 
+                top: "-15px",
+                width: "45px",
+                height: "36px",
+                opacity: 0.85
+              }}
+            />
+            <p className="mission-description" style={{ 
+              fontStyle: 'normal',
+              paddingLeft: '30px',
+              position: 'relative'
+            }}>
             Our mission is to provide flawless technical diamonds that drive innovation across industries, from next-generation electronics to life-changing quantum solutions.
             </p>
           </div>
