6eb86f62ee3479559917cb58d87cff641407bb63
AnonioZ <anton.zubaha@gmail.com>
2025-04-18 11:31:58 +0300

    Fix grafik and color of date

--- src/components/sections/ClientsSection.tsx
+++ src/components/sections/ClientsSection.tsx
@@ -20,34 +20,52 @@ const ClientsSection: React.FC = () => {
 
   const clients = [
     { 
-      name: 'XYZ Electronics', 
-      description: 'Semiconductor equipment manufacturer',
-      industry: 'Electronics'
+      name: 'Applied Diamond, Inc.', 
+      description: 'Leading manufacturer of CVD diamond products',
+      industry: 'Manufacturing',
+      logo: '/images/client1.png'
     },
     { 
-      name: 'Quantum Tech', 
-      description: 'Quantum computing research',
-      industry: 'Research'
+      name: 'Michigan State University', 
+      description: 'Research institution with diamond technology development',
+      industry: 'Education & Research',
+      logo: '/images/client2.png'
     },
     { 
-      name: 'Advanced Materials Inc', 
-      description: 'Industrial diamond applications',
-      industry: 'Materials'
+      name: 'Argonne National Laboratory', 
+      description: 'U.S. Department of Energy multidisciplinary science and engineering research center',
+      industry: 'Research',
+      logo: '/images/client3.jpg'
     },
     { 
-      name: 'SemiCorp', 
-      description: 'Semiconductor fabrication',
-      industry: 'Manufacturing'
+      name: 'The European Synchrotron Radiation Facility', 
+      description: 'International research facility for cutting-edge research with photons',
+      industry: 'Research',
+      logo: '/images/client4.jpg'
     },
     { 
-      name: 'Opti-Wave', 
-      description: 'Optical component manufacturer',
-      industry: 'Optics'
+      name: 'HiQuTe Diamond', 
+      description: 'High-quality technical diamond solutions provider',
+      industry: 'Technology',
+      logo: '/images/client5.png'
     },
     { 
-      name: 'Diamond Research Lab', 
-      description: 'Advanced materials R&D',
-      industry: 'Research'
+      name: 'LSPM-CNRS', 
+      description: 'Laboratoire des Sciences des Procédés et des Matériaux',
+      industry: 'Research',
+      logo: '/images/client6.png'
+    },
+    { 
+      name: 'DIAMFAB', 
+      description: 'Diamond semiconductor device manufacturer',
+      industry: 'Electronics',
+      logo: '/images/client7.jpg'
+    },
+    { 
+      name: 'UC Santa Barbara', 
+      description: 'Leading research university with diamond materials research',
+      industry: 'Education & Research',
+      logo: '/images/client8.jpg'
     }
   ];
 
@@ -66,9 +84,11 @@ const ClientsSection: React.FC = () => {
               } as React.CSSProperties}
             >
               <div className="client-logo-container">
-                <div className="client-logo-placeholder">
-                  {client.name.charAt(0)}
-                </div>
+                <img 
+                  src={client.logo} 
+                  alt={`${client.name} logo`} 
+                  className="client-logo" 
+                />
               </div>
               <h3 className="client-name">{client.name}</h3>
               <p className="client-industry">{client.industry}</p>
